eb5e0df1a5e234cf9aed501b4449f1c9e789654c
Rowaa <rowaa.almogheer@gmail.com>
2020-06-18 22:48:01 +0200

    memory card

---
+++ app.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', () => {
+   //card options
+   const cardArray= [
+    {
+        name: 'cat',
+         img: 'image/cat.png',
+    }, 
+    {
+        name: 'cat',
+         img: 'image/cat.png',
+    },
+    {
+        name: 'bird',
+         img: 'image/bird.png',
+    },
+    {
+        name: 'bird',
+         img: 'image/bird.png',
+    },
+    {
+        name: 'fish',
+         img: 'image/fish.png',
+    },
+    {
+        name: 'fish',
+         img: 'image/fish.png',
+    },
+    {
+        name: 'horse',
+         img: 'image/horse.png',
+    },
+    {
+        name: 'horse',
+         img: 'image/horse.png',
+    },
+    {
+        name: 'monkey',
+         img: 'image/monkey.png',
+    },
+    {
+        name: 'monkey',
+         img: 'image/monkey.png',
+    },
+    
+   ]
+})
