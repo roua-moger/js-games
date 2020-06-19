d717ecaec34396d99342ebb0b186c12acb8447e9
Rowaa <rowaa.almogheer@gmail.com>
2020-06-19 21:31:15 +0200

    whack a mole game

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+//memory card
 document.addEventListener('DOMContentLoaded', () => {
    //card options
    const cardArray= [
@@ -106,10 +108,59 @@ cardChosenId.push(cardId)
 this.setAttribute('src', cardArray[cardId].img)
 if (cardChosen.length === 2 ){
     setTimeout(checkForMatch, 500)
+    }
 }
-}
-
 
 createBoard()
 
 })
+
+//whack a mole
+
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('.mole')
+const timeLeft = document.querySelector('#time-left')
+let score = document.querySelector('#score')
+
+let result = 0
+let currentTime = timeLeft.textContent
+
+function randomSquare() {
+  square.forEach(className => {
+      className.classList.remove('mole')
+  })  
+  let randomPosition = square[Math.floor(Math.random() * 8)]
+  randomPosition.classList.add('mole')
+
+  //assign the id of randomPosition to hit position for us to use later
+  hitPosition = randomPosition.id
+}
+
+square.forEach(id => {
+    id.addEventListener('mouseup', () => {
+     if(id.id === hitPosition) {
+         result = result + 1
+         score.textContent = result
+         
+     }   
+    })
+})
+
+function moveMole() {
+   let timerId = null
+   timerId = setInterval(randomSquare, 1000)  
+}
+
+moveMole()
+
+function countDown() {
+    currentTime --
+    timeLeft.textContent = currentTime
+  
+    if(currentTime === 0 ) {
+      clearInterval(timerId)
+      alert('GAME OVER! Your final score is ' + result)
+    }
+  }
+  
+  let timerId = setInterval(countDown, 1000)
